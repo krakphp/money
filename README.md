006640cda9b08d83046b9d9888c379c33afd48c7
RJ Garcia <rj@bighead.net>
2016-04-16 14:18:36 -0700

    adding tax tests and documentation
    Signed-off-by: RJ Garcia <rj@bighead.net>

--- README.md
+++ README.md
@@ -1,3 +1,56 @@
 # Money
 
 Simple money manipulation library which uses bcmath to manipulate money values appropriately.
+
+## Install
+
+```
+compose require krak/money
+```
+
+## Usage
+
+```php
+<?php
+
+use function Krak\Money\money_sum,
+    Krak\Money\money_diff,
+    Krak\Money\money_prod,
+    Krak\Money\money_quot,
+    Krak\Money\money_cmp;
+
+var_dump(money_cmp('6', money_sum('1.00', '2', '3.0')) === 0);
+// bool(true)
+
+var_dump(money_cmp('1.00', money_diff('8', '3', '4')) === 0);
+// bool(true)
+```
+
+## API
+
+```
+string money_diff(...$args)
+string money_sum(...$args)
+string money_prod(...$args)
+string money_quot(...$args)
+string money_tax_rate($total, $tax)
+string money_tax($total, $rate)
+int money_cmp($m1, $m2)
+
+```
+
+`diff -> difference`, `prod -> product`, `quot -> quotient`, `cmp -> compare`
+
+`money_diff` subtracts each of the values from left to right
+
+`money_sum` sums all of the values together
+
+`money_prod` multiplies all of the values together
+
+`money_quot` divides all of the values together
+
+`money_cmp` returns a comparison of the two money strings using `bccmp`
+
+`money_tax_rate` calculates out the effective tax rate between a total and tax amount
+
+`money_tax` calculates the tax amount from a total and tax rate
--- test/money.php
+++ test/money.php
@@ -4,7 +4,9 @@ use function Krak\Money\money_sum,
     Krak\Money\money_cmp,
     Krak\Money\money_diff,
     Krak\Money\money_prod,
-    Krak\Money\money_quot;
+    Krak\Money\money_quot,
+    Krak\Money\money_tax_rate,
+    Krak\Money\money_tax;
 
 describe('Money', function() {
     describe('#money_sum', function() {
@@ -14,7 +16,7 @@ describe('Money', function() {
     });
     describe('#money_diff', function() {
         it('takes operands and subtracts them', function() {
-           assert(money_cmp('0.00', money_diff('1', '0.5', '0.5')) === 0); 
+           assert(money_cmp('0.00', money_diff('1', '0.5', '0.5')) === 0);
         });
     });
     describe('#money_prod', function() {
@@ -25,6 +27,16 @@ describe('Money', function() {
     describe('#money_quot', function() {
         it('takes operands and divides them', function() {
             assert(money_cmp('1', money_quot('4', '2', '2') === 0));
+        });
+    });
+    describe('#money_tax_rate', function() {
+        it('calculates the tax rate from a total and tax amount', function() {
+            assert(money_cmp('0.1', money_tax_rate('10.00', '1')) === 0);
+        });
+    });
+    describe('#money_tax', function() {
+        it('calculates the tax from a total and tax rate', function() {
+            assert(money_cmp('1', money_tax('10.00', '0.1')) === 0);
         });
     });
 });
